8d9243f8c3974557c6ddef0b54c7d34b97ba9e97
Kevin Dahlhausen <kevin.dahlhausen@gmail.com>
2016-08-09 16:08:21 -0400

    added README, .gitignore

---
+++ README.mk
@@ -0,0 +1,5 @@
+
+#To Build:
+In order to build for Android you must enable the CSV library.  Do this by editing each of the following files and removing the line containing _csv:
+1 ./.buildozer/android/platform/python-for-android/src/blacklist.txt
+2 ./.buildozer/android/platform/python-for-android/dist/stockwatch/blacklist.txt
