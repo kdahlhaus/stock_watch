c3fc073b58dd90892806563216a5657c4f30e371
Kevin Dahlhausen <kevin.dahlhausen@gmail.com>
2016-08-25 16:27:00 -0400

    refactored stock price provider

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, StringProperty
 from kivy.storage.jsonstore import JsonStore
-from kivy.network.urlrequest import UrlRequest
 from kivy.core.window import Window
 from kivy.uix.behaviors.focus import FocusBehavior  #https://kivy.org/docs/api-kivy.uix.behaviors.focus.html
 from kivy.utils import platform
@@ -16,8 +15,7 @@ from Datagrid import DataGrid
 from price_input import PriceInput
 from integer_input import IntegerInput
 
-import csv
-
+from stock_price_providers.yahoo_provider import YahooStockPriceProvider
 
 if platform=="android":
     DATA_GRID_ROW_HEIGHT = 80
@@ -40,53 +38,6 @@ class StockEntry(object):
         
 
 
-class YahooStockPrices():
-
-    def __init__(self, on_results=None, on_error=None):
-        self.symbols = []
-        self.on_results=on_results
-        self.on_error=on_error
-        self.prices = {}
-
-    def add_symbol(self, symbol):
-        self.symbols.append(symbol)
-
-    def get_prices(self):
-        if len(self.symbols)>0:
-            symbol_param=""
-            for symbol in self.symbols:
-                symbol_param += "+" + symbol
-            symbol_param = symbol_param[1:]
-            self.req = UrlRequest("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=sbagh"%symbol_param, 
-                              on_success=self.got_yahoo_prices, 
-                              on_failure=self.got_yahoo_error, 
-                              on_error=self.got_yahoo_error,
-                              on_redirect=self.on_redirect,
-                              )
-
-    def got_yahoo_prices(self, request, results):
-        #print results, len(results), type(results)
-        quote_reader = csv.reader(results.splitlines())
-        for row in quote_reader:
-            self.prices[row[0]]=row
-        if self.on_results:
-            self.on_results(self.prices)
-
-
-
-    def got_yahoo_error(self, request, results=None):
-        print "error: %s %s"%(request, results)
-        if self.on_error:
-            self.on_error("error " + str(request)+" "+str(results))
-
-
-    def on_redirect(self, *args):
-        print "on_redirect:"+str(args)
-        if self.on_error:
-            self.on_error("redirect " + str(args))
-
-
- 
 
 class MainWindow(TabbedPanel):
 
@@ -202,7 +153,7 @@ class StockWatch(App):
 
     def refresh_prices(self):
         "kick off async call to get updated stock proces"
-        self.pricer = YahooStockPrices(self.on_new_prices, self.on_error)
+        self.pricer = YahooStockPriceProvider(self.on_new_prices, self.on_error)
         symbols = set()
         for stock_entry in self.stock_entries:
             symbols.add(stock_entry.symbol)
---
+++ stock_price_providers/yahoo_provider.py
@@ -0,0 +1,49 @@
+from kivy.network.urlrequest import UrlRequest
+
+import csv
+
+
+class YahooStockPriceProvider():
+
+    def __init__(self, on_results=None, on_error=None):
+        self.symbols = []
+        self.on_results=on_results
+        self.on_error=on_error
+        self.prices = {}
+
+    def add_symbol(self, symbol):
+        self.symbols.append(symbol)
+
+    def get_prices(self):
+        if len(self.symbols)>0:
+            symbol_param=""
+            for symbol in self.symbols:
+                symbol_param += "+" + symbol
+            symbol_param = symbol_param[1:]
+            self.req = UrlRequest("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=sbagh"%symbol_param, 
+                              on_success=self.got_yahoo_prices, 
+                              on_failure=self.got_yahoo_error, 
+                              on_error=self.got_yahoo_error,
+                              on_redirect=self.on_redirect,
+                              )
+
+    def got_yahoo_prices(self, request, results):
+        #print results, len(results), type(results)
+        quote_reader = csv.reader(results.splitlines())
+        for row in quote_reader:
+            self.prices[row[0]]=row
+        if self.on_results:
+            self.on_results(self.prices)
+
+
+
+    def got_yahoo_error(self, request, results=None):
+        print "error: %s %s"%(request, results)
+        if self.on_error:
+            self.on_error("error " + str(request)+" "+str(results))
+
+
+    def on_redirect(self, *args):
+        print "on_redirect:"+str(args)
+        if self.on_error:
+            self.on_error("redirect " + str(args))
